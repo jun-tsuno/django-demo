2afbd0d5aa87ceb1d707cb75c4579a6cc6199729
Jun <jun55tsuno@gmail.com>
2024-01-10 22:59:06 -0800

    updated

--- challenges/views.py
+++ challenges/views.py
@@ -46,8 +46,9 @@ def monthly_challenges_by_number(request, month):
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
-    response_data = f"<h1>{challenge_text}</h1>"
-
-    return HttpResponse(response_data)
+    return render(request, "challenges/challenge.html", {
+      "text": challenge_text,
+      "month_name": month
+    })
   except:
     return HttpResponseNotFound("This month is not supported")
